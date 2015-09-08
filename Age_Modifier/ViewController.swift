db281d15e91e7ad41062a54af3888a1bd81de570
James Tatum <jtatum@gmail.com>
2015-09-07 17:27:51 -0700

    Make everything spin
    I added an outlet for the button and extended UIView with a
    Rotate360Degrees method. Then I added a timer and some other
    junk and here you go!

--- Age_Modifier/ViewController.swift
+++ Age_Modifier/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
     // this is where you enter the text in human years
     @IBOutlet weak var ageTextField: UITextField!
 
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var flashLabel: UILabel!
     //age converson button
     @IBAction func button(sender: AnyObject) {
 
@@ -30,15 +33,24 @@ class ViewController: UIViewController {
         ageTextField.resignFirstResponder()
         lumpYearA.text = "In Lumpy Cordoza years that's;"
         lumpYearB.text = "200"
-
     }
 
-
+    func flash() {
+        flashLabel.hidden = !flashLabel.hidden
+        ageTextField.rotate360Degrees()
+        lumpYearA.rotate360Degrees()
+        lumpYearB.rotate360Degrees()
+        image.rotate360Degrees()
+        flashLabel.rotate360Degrees()
+    }
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        image.rotate360Degrees()
+        let interval: CFTimeInterval = 1
+        NSTimer.scheduledTimerWithTimeInterval(interval, target: self, selector: Selector("flash"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
